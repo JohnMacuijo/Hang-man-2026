60f7aea08e82567e7ddfb3291106d79adddcb9fd
JohnMacuijo <ejks@passerellesnumeriques.org>
2026-07-17 03:31:43 +0000

    Adding Pictures and Responsive ui

--- src/main/java/fr/quentincillierre/hangman/application/MainApp.java
+++ src/main/java/fr/quentincillierre/hangman/application/MainApp.java
@@ -10,14 +10,21 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("game-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fr/quentincillierre/hangman/application/game-view.fxml"));
         Parent root = loader.load();
 
+        // 1. Scene setup
         Scene scene = new Scene(root, 950, 850);
+
+        // 2. Set Minimum Dimensions (prevents UI from collapsing too small)
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(750);
+
+        // 3. Enable resizing & standard window controls
+        primaryStage.setResizable(true); 
 
         primaryStage.setTitle("Neon Hangman");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
